2457746b2a706c4bddc0b063183d20e3e30e474e
Som2486 <mindflair2486@gmail.com>
2023-08-10 19:38:01 +0530

    Code to find pallindrome string

--- pallindrome.java
+++ pallindrome.java
@@ -4,7 +4,21 @@ public class pallindrome{
      public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         String s= sc.nextLine();
+        int l=s.length();
+        String s1="";
+        while(l>0)
+        {
+            s1=s1+s.charAt(l-1);
+            l=l-1;
+
+        }
+        if(s.equals(s1))
+        {
+            System.out.println("Pallindrome String");
         
+        }
+        else
+        System.out.println("Not a Pallindrome");
         
     }
 }
