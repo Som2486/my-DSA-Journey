08321979a7bbdf63d0cfd5b723e33020cc76f712
Som2486 <mindflair2486@gmail.com>
2024-02-26 12:52:25 +0530

    Update hcf.java

---
+++ hcf.java
@@ -0,0 +1,18 @@
+public class hcf {
+    public static void main(String args[])
+    {
+        int a=70;
+        int b=15, hcf=0;
+        for(int i=1; i<=a || i<=b; i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                hcf=i;
+               
+            }
+            
+        }
+         System.out.println(i);
+    }
+    
+}
