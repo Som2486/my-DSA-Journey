f45b7782b32ba11ab25b53294789961a6922d8b7
Som2486 <mindflair2486@gmail.com>
2024-02-26 12:53:24 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,17 @@
+public class max {
+    public static void main(String args[])
+    {
+        int arr[]={0,-1,2,0,5};
+        int n=arr.length;
+        
+        for(int i=0;i<=n;i++)
+        {
+
+        }
+    }
+
+
+
+    
+    
+}
