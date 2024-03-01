ebb84beb0dd281acc9e03186bcbbb13768f179c9
Som2486 <mindflair2486@gmail.com>
2024-03-01 22:35:41 +0530

    Create maxConcecutive.java

---
+++ maxConcecutive.java
@@ -0,0 +1,22 @@
+public class maxConcecutive {
+    public static void main(String args[])
+    {
+        int arr[]={1,0,0,0,1,1,1,1,1,0,5,3,2,1,1};
+        int count=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==1)
+            {
+                count++;
+                max=Math.max(max,count);
+            }
+            else{
+            count=0;
+            }
+        }
+        System.out.print(max);
+
+    }
+    
+}
