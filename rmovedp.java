b4a530578a38270173523687b0c3b83f4b9dc389
Som2486 <mindflair2486@gmail.com>
2024-03-08 19:34:24 +0530

    Update rmovedp.java

--- rmovedp.java
+++ rmovedp.java
@@ -2,7 +2,7 @@ public class rmovedp {
     public static void main(String[] args) {
 		int arr[]={1,1,2,2,3,3};
 		int i=0;
-		for(int j=1;i<arr.length;j++)
+		for(int j=0;i<arr.length;j++)
 		{
 		    if(arr[i]!=arr[j]){
 		    arr[i+1]=arr[j];
